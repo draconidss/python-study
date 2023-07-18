6078d7c804b9ae437abc13cc29904becf6bc3796
Draco <1138312802@qq.com>
2023-07-19 00:01:24 +0800

    2023-07-19 00:01:19

--- src/datastruct/number.py
+++ src/datastruct/number.py
@@ -3,6 +3,12 @@ import unittest
 
 class String(unittest.TestCase):
     def test_operator(self):
-        # 乘方运算两个 **
-        print(3**2) # 9
+        # 乘方运算用两个 **
+        print(f"乘方运算：", 3 ** 2)  # 9
+        # 除法运算总是得到浮点数
+        print(f"除法运算总是得到浮点数：", 4 / 2)  # 2.0
+        # 除法运算得到浮点数，但结果无法保证精确
+        print(f"除法运算不精确：", 10 / 3)  # 3.3333333333333335
+        # 浮点数运算结果无法保证精确
+        print(f"浮点数运算结果无法保证精确：", 0.1 + 0.2, )  # 0.30000000000000004
         self.assertEqual(True, True)
--- src/datastruct/string.py
+++ src/datastruct/string.py
@@ -9,13 +9,33 @@ class String(unittest.TestCase):
         print(s.upper())
         print(s.lower())
         print(s.rstrip())
-        print()
+        print(len(s))
         self.assertEqual(True, True)
 
     def test_format(self):
         s = "hello {name}"
-        print(s.format(name="world"))
+        print(s.format(name="world")) # hello world
 
         place = """world"""
-        print(f"hello {place}")
+        print(f"hello {place}") # hello world
+
+        foo = "bar"
+        print(f"{foo=}") # foo='bar'，3.8 新特性
+        self.assertEqual(True, True)
+
+    def test_slice(self):
+        s = "hello world"
+        print(s[0:5]) # hello, 从0开始，到5结束，不包括5
+        print(s[6:]) # world, 从6开始，到结尾
+        print(s[:5]) # hello, 从0开始，到5结束，不包括5
+        print(s[-5:]) # world, 从倒数第5个开始，到结尾
+        print(s[:-5]) # hello, 从0开始，到倒数第5个结束，不包括倒数第5个
+        print(s[::2]) # hlowrd, 从0开始，到结尾，每隔2个取一个
+        print(s[::-2]) # drwlh, 从结尾开始，到0，每隔2个取一个
+        print(s[-2::-2]) # lo le, 从倒数第2个开始，到0，每隔2个取一个
+        # 自动处理越界索引
+        print(s[0:100]) # hello world
+        print(s[-100:100]) # hello world
+        s1 = 'a'
+        print("s1[0]:", s1[-1])
         self.assertEqual(True, True)
